890291881de6e5a23b21dcad02fd70b7ba35e63f
Saumya Singh <singhsaumyait034@gmail.com>
2024-06-17 17:40:27 +0530

    Added more files

---
+++ src/test/java/stepDefs/BankLoginPageStepDef.java
@@ -0,0 +1,50 @@
+package stepDefs;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDef {
+    @Given("user is on the login page")
+    public void userIsOnTheLoginPage() {
+
+
+    }
+
+    @When("user gets the title of the page")
+    public void userGetsTheTitleOfThePage() {
+    }
+
+    @Then("{string} link should be displayed")
+    public void linkShouldBeDisplayed(String arg0) {
+
+    }
+
+    @When("user enters username as {string}")
+    public void userEntersUsernameAs(String arg0) {
+
+    }
+
+    @And("user enters password as {string}")
+    public void userEntersPasswordAs(String arg0) {
+
+    }
+
+    @And("user clicks on login button")
+    public void userClicksOnLoginButton() {
+
+    }
+
+    @And("page title should be {string}")
+    public void pageTitleShouldBe(String arg0) {
+
+    }
+
+    @Then("title of page should be {string}")
+    public void titleOfPageShouldBe(String arg0) {
+    }
+
+
+
+}
