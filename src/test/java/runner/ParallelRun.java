3de20d489ee02722ee65f7d7c0eac7b92241543c
Saumya Singh <singhsaumyait034@gmail.com>
2024-07-08 16:56:38 +0530

    Added parallel Execution

---
+++ src/test/java/runner/ParallelRun.java
@@ -0,0 +1,19 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/resources/feature"},
+        glue = {"stepDefs", "appHooks"},
+        plugin = {"pretty",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "timeline:test-output-thread/"
+        }
+)
+
+
+public class ParallelRun {
+}
---
+++ test-output-thread/report.js
@@ -0,0 +1,98 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "cd89b799-09ef-4fbd-880a-3155107f1689",
+    "feature": "Products List Feature",
+    "scenario": "User add product into the cart",
+    "start": 1720437858022,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1720437861246,
+    "className": "failed"
+  },
+  {
+    "id": "7a8e0538-e275-4ae6-89b0-837068c25da8",
+    "feature": "Products List Feature",
+    "scenario": "User verify products Menu button",
+    "start": 1720437861249,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1720437864086,
+    "className": "failed"
+  },
+  {
+    "id": "a5936afa-a6da-4699-9ee7-ef8b01c41634",
+    "feature": "Account Service Feature",
+    "scenario": "Verify Account Services Section",
+    "start": 1720437843663,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1720437847124,
+    "className": "failed"
+  },
+  {
+    "id": "8c95be79-d125-4ab5-a702-c532caeba1b2",
+    "feature": "Bank Login Page Feature",
+    "scenario": "Verify Login Page title",
+    "start": 1720437847129,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1720437849472,
+    "className": "passed"
+  },
+  {
+    "id": "d148ed50-9139-430b-8a63-a30623b1faa7",
+    "feature": "Account Service Feature",
+    "scenario": "Verify user is on the Account Service Page",
+    "start": 1720437840371,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1720437843652,
+    "className": "failed"
+  },
+  {
+    "id": "9695b59e-269a-4e6b-8245-b8fffe54d767",
+    "feature": "Products List Feature",
+    "scenario": "User will get the list of all products",
+    "start": 1720437852123,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1720437855514,
+    "className": "passed"
+  },
+  {
+    "id": "a7eb173c-3585-4c96-bef0-fa9a61c6ac80",
+    "feature": "Bank Login Page Feature",
+    "scenario": "Verify User is able to login with valid credentials",
+    "start": 1720437849480,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1720437852118,
+    "className": "passed"
+  },
+  {
+    "id": "343ab14b-360d-4cd5-ac9d-f79db9e2dea8",
+    "feature": "Products List Feature",
+    "scenario": "User gets the list of all products",
+    "start": 1720437855518,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1720437858019,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[#1,main,5,main]"
+  }
+]);
+});
