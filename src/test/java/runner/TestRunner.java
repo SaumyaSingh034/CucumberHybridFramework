b324e23d7bf95bd3db0bb38eccb1d141795f19b1
Saumya Singh <singhsaumyait034@gmail.com>
2024-08-30 13:14:16 +0530

    Scenario Outline

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,7 +8,6 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = {"src/resources/feature"},
         glue = {"stepDefs", "appHooks"},
-        tags = {"~@Sanity"},
         plugin = {"pretty",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "timeline:test-output-thread/"
